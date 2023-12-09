cf171e72538bd00534beddb44ba97da6daeedd2b
Andrey Mitin <amitin@gomodus.com>
2023-12-09 15:57:53 -0600

    Eliminate unnecessary intermediate script match.sh

--- upload.js
+++ upload.js
@@ -29,7 +29,7 @@ router.post("/", async (request, response) => {
     fs.writeFileSync(txtFilePath, txt);
 
     // -u means urgent: the task is given priority over other queued tasks
-    const {stdout: jobIdLine} = await executeCommand(`ts sh sh/match.sh ${relPath} ${txtFilePath} tiny`);
+    const {stdout: jobIdLine} = await executeCommand(`ts sh docker.sh ${relPath} tiny 16 ${txtFilePath}`);
     executeCommand(`ts -u ${jobIdLine}`).then().catch();
 
     response.redirect(txtPublicFilePath);
