98e3a6243ce1a20d888be4b4cf3a7c12bb6d1a82
Andrey Mitin <andrey.a.mitin@gmail.com>
2024-08-18 21:16:41 -0500

    Fix: use a standard extension for the filename or specify the format manually

--- upload.js
+++ upload.js
@@ -40,8 +40,8 @@ async function transcribe(audio, lang, modelSize, host, time, interval) {
     if (time) {
       const ss = `$(TZ=UTC date -d "2024-01-01T${time}Z - ${interval} seconds" +%H:%M:%S)`;
       const to = `$(TZ=UTC date -d "2024-01-01T${time}Z + ${interval} seconds" +%H:%M:%S)`;
-      filename = `${md5}-${time}-${interval}.mp3`;
-      await executeCommand(`ffmpeg -ss ${ss} -to ${to} -i tmp/${md5} -c copy tmp/${filename}`);
+      filename = `${md5}-${time}-${interval}`;
+      await executeCommand(`ffmpeg -ss ${ss} -to ${to} -i tmp/${md5} -c copy -f mp3 tmp/${filename}`);
     }
 
     const txtPublicFilePath = `lookups/${md5}.txt`;
